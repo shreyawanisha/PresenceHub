20cf6369f36a2369b33e91be30e2c464b75cd789
shreyawanisha <wanisha.s@northeastern.edu>
2025-03-21 23:59:37 -0400

    Initial project setup with Spring Boot, Maven config, and .gitignore

---
+++ src/main/java/org/attendencemanagement/attendencemanagementsystem/AttendenceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.attendencemanagement.attendencemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendenceManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendenceManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/attendencemanagement/attendencemanagementsystem/AttendenceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.attendencemanagement.attendencemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendenceManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
