6d575cad1878b0dc0c5281bac58112d66313e5f2
shreyawanisha <wanisha.s@northeastern.edu>
2025-03-30 02:18:45 -0400

    Add JWTUtil.java

---
+++ src/main/java/org/attendance/util/JwtUtil.java
@@ -0,0 +1,32 @@
+package org.attendance.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String subject){
+        long expirationTime = 1000 * 60 * 60; // 1 hour
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSigningKey())
+                .compact();
+    }
+}
