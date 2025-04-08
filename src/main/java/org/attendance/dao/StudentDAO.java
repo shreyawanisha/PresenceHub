64bd1d0d78471f079941d4f12f46913054cd530b
shreyawanisha <wanisha.s@northeastern.edu>
2025-04-08 03:51:57 -0400

    add StudentDAO and StudentDAOImpl for student data access methods

---
+++ src/main/java/org/attendance/dao/StudentDAO.java
@@ -0,0 +1,10 @@
+package org.attendance.dao;
+
+import org.attendance.entity.Student;
+
+import java.util.Optional;
+
+public interface StudentDAO extends GenericDAO<Student>{
+    Optional<Student> findByUserId(long userId);
+    Optional<Student> findByRollNumber(String rollNumber);
+}
---
+++ src/main/java/org/attendance/dao/StudentDAOImpl.java
@@ -0,0 +1,27 @@
+package org.attendance.dao;
+
+import org.attendance.entity.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class StudentDAOImpl extends GenericDAOImpl<Student> implements StudentDAO {
+    public StudentDAOImpl() {
+        super(Student.class);
+    }
+
+    @Override
+    public Optional<Student> findByUserId(long userId) {
+        return getSession().createQuery("SELECT s FROM Student s WHERE s.user.id = :userId", Student.class)
+                .setParameter("userId", userId)
+                .uniqueResultOptional();
+    }
+
+    @Override
+    public Optional<Student> findByRollNumber(String rollNumber) {
+        return getSession().createQuery("SELECT s FROM Student s WHERE s.rollNumber = :rollNumber", Student.class)
+                .setParameter("rollNumber", rollNumber)
+                .uniqueResultOptional();
+    }
+}
