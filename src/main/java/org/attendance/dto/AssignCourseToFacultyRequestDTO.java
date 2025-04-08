2550ae472d41998529ed38fa8650609a4d209ec9
shreyawanisha <wanisha.s@northeastern.edu>
2025-04-08 02:12:43 -0400

    add AssignCourseToFacultyRequestDTO

---
+++ src/main/java/org/attendance/dto/AssignCourseToFacultyRequestDTO.java
@@ -0,0 +1,41 @@
+package org.attendance.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public class AssignCourseToFacultyRequestDTO {
+
+    @NotNull(message = "Course ID is required")
+    @Min(value = 1, message = "Course ID must be greater than 0")
+    private Long courseId;
+
+    @NotEmpty(message = "Faculty IDs list must not be empty")
+    private List<Long> facultyIds;
+
+    public AssignCourseToFacultyRequestDTO() {
+    }
+
+    public AssignCourseToFacultyRequestDTO(Long courseId, List<Long> facultyIds) {
+        this.courseId = courseId;
+        this.facultyIds = facultyIds;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public List<Long> getFacultyIds() {
+        return facultyIds;
+    }
+
+    public void setFacultyIds(List<Long> facultyIds) {
+        this.facultyIds = facultyIds;
+    }
+}
