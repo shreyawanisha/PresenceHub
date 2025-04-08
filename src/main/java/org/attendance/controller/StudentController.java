a4c6d8a1302bf97ba859ff30f8d483609459b713
shreyawanisha <wanisha.s@northeastern.edu>
2025-04-08 03:52:13 -0400

    add StudentController for managing student registration and retrieval

---
+++ src/main/java/org/attendance/controller/StudentController.java
@@ -0,0 +1,59 @@
+package org.attendance.controller;
+
+import org.attendance.dto.request.StudentRequestDTO;
+import org.attendance.dto.response.ApiResponse;
+import org.attendance.dto.response.StudentResponseDTO;
+import org.attendance.service.interfaces.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<?> registerStudent(@RequestBody StudentRequestDTO dto) {
+            studentService.registerStudent(dto);
+            String message = String.format(
+                    "Student registered successfully:\nUserId: %d\nRollNumber: %s\nDepartment: %s\nSemester: %s",
+                    dto.getUserId(), dto.getRollNumber(), dto.getDepartment(), dto.getSemester()
+            );
+            return ResponseEntity.ok(new ApiResponse(HttpStatus.OK.value(), message));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<?> getAllStudents() {
+        try {
+            List<StudentResponseDTO> allStudents = studentService.getAllStudents();
+            return ResponseEntity.ok(allStudents);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error fetching students", e);
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getStudentByUserId(@PathVariable Long userId) {
+        try {
+            StudentResponseDTO dto = studentService.getByUserId(userId);
+            return ResponseEntity.ok(dto);
+        } catch (ResponseStatusException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error fetching student", e);
+        }
+    }
+}
