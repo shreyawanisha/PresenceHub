c03aba8f9f170a01e8c18f333792c4f5fa5ac500
shreyawanisha <wanisha.s@northeastern.edu>
2025-04-08 18:46:27 -0400

    add EnrollmentDAO and EnrollmentDAOImpl for enrollment data access

---
+++ src/main/java/org/attendance/dao/EnrollmentDAO.java
@@ -0,0 +1,13 @@
+package org.attendance.dao;
+
+import org.attendance.entity.Enrollment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EnrollmentDAO extends GenericDAO<Enrollment> {
+    Optional<Enrollment> findByStudentAndCourse(long studentId, long courseId);
+    List<Enrollment> findByCourseId(long courseId);
+    List<Enrollment> findByStudentId(long studentId);
+
+}
---
+++ src/main/java/org/attendance/dao/EnrollmentDAOImpl.java
@@ -0,0 +1,40 @@
+package org.attendance.dao;
+
+import jakarta.persistence.NoResultException;
+import org.attendance.entity.Enrollment;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class EnrollmentDAOImpl extends GenericDAOImpl<Enrollment> implements EnrollmentDAO {
+
+    public EnrollmentDAOImpl() {
+        super(Enrollment.class);
+    }
+
+    @Override
+    public Optional<Enrollment> findByStudentAndCourse(long studentId, long courseId) {
+        try {
+            final Enrollment enrollment = getSession().createQuery("FROM Enrollment e WHERE e.student.id = :studnetId AND e.course.id = :courseId", Enrollment.class)
+                    .setParameter("studentId", studentId)
+                    .setParameter("courseId", courseId)
+                    .getSingleResult();
+            return Optional.of(enrollment);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Enrollment> findByCourseId(long courseId) {
+        return getSession().createQuery("FROM Enrollment e where e.course.id = :courseId", Enrollment.class)
+                .setParameter("courseId", courseId).getResultList();
+    }
+
+    @Override
+    public List<Enrollment> findByStudentId(long studentId) {
+        return getSession().createQuery("FROM Enrollment e where e.student.id = :studentId", Enrollment.class).setParameter("studentId", studentId).getResultList();
+    }
+}
