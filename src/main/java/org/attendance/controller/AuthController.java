0d643beec15bbd91df3000fb5eee270dab35a929
shreyawanisha <wanisha.s@northeastern.edu>
2025-04-14 02:56:26 -0400

    add endpoints to retrieve unassigned students and faculties for admin

--- src/main/java/org/attendance/controller/AuthController.java
+++ src/main/java/org/attendance/controller/AuthController.java
@@ -1,17 +1,22 @@
 package org.attendance.controller;
 
 import jakarta.validation.Valid;
+import org.attendance.dto.response.UserResponseDTO;
 import org.attendance.dto.request.LoginRequestDTO;
 import org.attendance.dto.request.RegistrationRequestDTO;
 import org.attendance.dto.response.ApiResponse;
 import org.attendance.dto.response.AuthResponseDTO;
+import org.attendance.entity.User;
 import org.attendance.service.interfaces.UserService;
 import org.attendance.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -63,5 +68,23 @@ public class AuthController {
     @PostMapping("/logout")
     public ResponseEntity<?> logout() {
         return ResponseEntity.ok(new ApiResponse(HttpStatus.NO_CONTENT.value(), "You have been logged out successfully."));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/unassigned-students")
+    public ResponseEntity<List<UserResponseDTO>> getUnassignedStudentUsers() {
+        List<User> users = userService.getUnassignedStudentUsers();
+        List<UserResponseDTO> userResponseDTOS = users.stream().map(UserResponseDTO::new).toList();
+
+        return ResponseEntity.ok(userResponseDTOS);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/unassigned-faculties")
+    public ResponseEntity<List<UserResponseDTO>> getUnassignedFacultyUsers() {
+        List<User> users = userService.getUnassignedFacultyUsers();
+        List<UserResponseDTO> userResponseDTOS = users.stream().map(UserResponseDTO::new).toList();
+
+        return ResponseEntity.ok(userResponseDTOS);
     }
 }
