25289bfd483e4ba33aa0e5d0e7d812ce59e2e8c7
shreyawanisha <wanisha.s@northeastern.edu>
2025-04-15 19:26:41 -0400

    add AttendanceRecordDTO for attendance record management

---
+++ src/main/java/org/attendance/dto/response/AttendanceRecordDTO.java
@@ -0,0 +1,53 @@
+package org.attendance.dto.response;
+
+import org.attendance.enums.AttendanceStatus;
+
+public class AttendanceRecordDTO {
+    private Long attendanceId;
+    private String username;
+    private String email;
+    private String rollNumber;
+    private AttendanceStatus status;
+
+    public AttendanceRecordDTO() {}
+
+    public Long getAttendanceId() {
+        return attendanceId;
+    }
+
+    public void setAttendanceId(Long attendanceId) {
+        this.attendanceId = attendanceId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public AttendanceStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AttendanceStatus status) {
+        this.status = status;
+    }
+}
