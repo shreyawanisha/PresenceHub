6d3472b904f3f2ad555d1e68b9eed34c7fcc1ab6
shreyawanisha <wanisha.s@northeastern.edu>
2025-04-15 19:46:23 -0400

    add AttendanceSummaryDTO for attendance summary data representation

---
+++ src/main/java/org/attendance/dto/response/AttendanceSummaryDTO.java
@@ -0,0 +1,68 @@
+package org.attendance.dto.response;
+
+public class AttendanceSummaryDTO {
+
+    private String courseName;
+    private String crn;
+    private String department;
+    private String semester;
+    private int totalClasses;
+    private int presentCount;
+    private double percentage;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCrn() {
+        return crn;
+    }
+
+    public void setCrn(String crn) {
+        this.crn = crn;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public int getTotalClasses() {
+        return totalClasses;
+    }
+
+    public void setTotalClasses(int totalClasses) {
+        this.totalClasses = totalClasses;
+    }
+
+    public int getPresentCount() {
+        return presentCount;
+    }
+
+    public void setPresentCount(int presentCount) {
+        this.presentCount = presentCount;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+}
